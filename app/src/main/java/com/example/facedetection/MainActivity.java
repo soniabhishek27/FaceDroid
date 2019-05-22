bb43bc90482c888cdfe56b89fc5e89803b620b5e
AbhisheksoniProjects <abhisheksoni3216@gmail.com>
2019-05-23 00:31:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/facedetection/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.facedetection;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.FaceDetector;
+import android.media.Image;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button camerabutton;
+    private final static int REQUEST_CAPTURE=123;
+    private FirebaseVisionImage image;
+    private FirebaseVisionFaceDetector detector;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FirebaseApp.initializeApp(this);
+        camerabutton=findViewById(R.id.camera_button);
+
+        camerabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if(takePictureIntent.resolveActivity(getPackageManager()) !=null)
+                {
+                    startActivityForResult(takePictureIntent, REQUEST_CAPTURE);
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==REQUEST_CAPTURE && resultCode==RESULT_OK)
+        {
+            Bundle extras= data.getExtras();
+            Bitmap bitmap=(Bitmap)extras.get("data");
+                detectface(bitmap);
+
+
+
+        }
+    }
+
+    private void detectface(Bitmap bitmap) {
+
+        FirebaseVisionFaceDetectorOptions options =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setModeType(FirebaseVisionFaceDetectorOptions.ACCURATE_MODE)
+                        .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationType(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .setMinFaceSize(0.15f)
+                        .setTrackingEnabled(true)
+
+                        .build();
+
+        try {
+            image=FirebaseVisionImage.fromBitmap(bitmap);
+            detector=FirebaseVision.getInstance()
+                    .getVisionFaceDetector(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+                String resultText="";
+                int i=1;
+                for(FirebaseVisionFace face: firebaseVisionFaces)
+                {
+                    resultText= resultText.concat("\n"+i+".")
+                            .concat("\nSmile"+face.getSmilingProbability()*100+"%")
+                            .concat("\nLeftEye"+face.getLeftEyeOpenProbability()+"%")
+                            .concat("\nRightEye"+face.getRightEyeOpenProbability()+"%")
+                            .concat("\nHeadPosition"+face.getHeadEulerAngleY()*100+"%")
+                            .concat("\nHeadPosition"+face.getHeadEulerAngleZ()*100+"%");
+
+                    i++;
+
+                }
+                if(firebaseVisionFaces.size()==0)
+                {
+                    Toast.makeText(MainActivity.this,"No Faces",Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                {
+                    Bundle bundle= new Bundle();
+                    bundle.putString(facedetection.RESULT_TEXT,resultText);
+                    DialogFragment resultDialog= new ResultDialog();
+                    resultDialog.setArguments(bundle);
+                    resultDialog.setCancelable(false);
+                    resultDialog.show(getSupportFragmentManager(),facedetection.RESULT_DIALOG );
+                }
+
+
+
+            }
+        });
+
+    }
+}
